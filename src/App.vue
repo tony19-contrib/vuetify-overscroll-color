e00d93dc43e7202dfbdc8b1b6044c5b427ed44ce
Tony Trinh <tony19@gmail.com>
2021-07-29 16:11:15 -0500

    demo vuetify theme on body background

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,27 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+  <v-app ref="app">
     <v-main>
-      <HelloWorld/>
+      <ThemeSwitcher />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import ThemeSwitcher from './components/ThemeSwitcher';
 
 export default {
   name: 'App',
-
   components: {
-    HelloWorld,
+    ThemeSwitcher,
   },
-
-  data: () => ({
-    //
-  }),
-};
+  watch: {
+    '$vuetify.theme.isDark'() {
+      this.$nextTick(() => {
+        const rootStyles = window.getComputedStyle(this.$refs.app.$el)
+        console.log('backgroundColor', rootStyles.backgroundColor)
+        document.body.style.backgroundColor = rootStyles.backgroundColor
+      })
+    }
+  }
+}
 </script>
